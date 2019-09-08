938ddd31957e9c1d8383b1192b82275b53e35566
David <davidzwa@gmail.com>
2019-09-08 16:36:41 +0200

    Stash2 commit

--- src/app/module/ng-mat-list-swipe/config/gesture-config.ts
+++ src/app/module/ng-mat-list-swipe/config/gesture-config.ts
@@ -1,5 +1,7 @@
 import {HammerGestureConfig} from '@angular/platform-browser';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class GestureConfig extends HammerGestureConfig {
     overrides = {
         pan: {
